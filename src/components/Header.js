4446e71f36b19b64411584131a339bccbf30f22c
emersonfbarros <emersonfb99@gmail.com>
2023-05-08 10:16:36 -0300

    feat: add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header className="header">
+        <h1 className="header-title">Solar System</h1>
+        <div className="ellipse" />
+      </header>
+    );
+  }
+}
+
+export default Header;
