26601d6402082d58e3bfe99911e6a605245a62ee
emersonfbarros <emersonfb99@gmail.com>
2023-05-08 11:14:43 -0300

    feat: render MissionCard inside Missions

--- src/components/Missions.js
+++ src/components/Missions.js
@@ -1,11 +1,26 @@
 import { Component } from 'react';
 import Title from './Title';
+import MissionCard from './MissionCard';
+import missions from '../data/missions';
 
 class Missions extends Component {
   render() {
     return (
       <div className="missions">
         <Title headline="Missions" />
+        <div className="cards">
+          {
+            missions.map((mission) => (
+              <MissionCard
+                key={ mission.name }
+                name={ mission.name }
+                year={ mission.year }
+                country={ mission.country }
+                destination={ mission.destination }
+              />
+            ))
+          }
+        </div>
       </div>
     );
   }
