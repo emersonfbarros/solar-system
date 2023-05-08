ac0614df593986c2b14ef8cc2e6e9e698361559e
emersonfbarros <emersonfb99@gmail.com>
2023-05-08 10:32:56 -0300

    render title and reafactor env

--- src/components/SolarSytem.js
+++ src/components/SolarSytem.js
@@ -1,9 +1,12 @@
 import { Component } from 'react';
+import Title from './Title';
 
 class SolarSystem extends Component {
   render() {
     return (
-      <div className="solar-system" />
+      <div className="solar-system">
+        <Title headline="Planets" />
+      </div>
     );
   }
 }
