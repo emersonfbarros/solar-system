05d6e923c8d2f3413f1557701736e7ed553933c5
emersonfbarros <emersonfb99@gmail.com>
2023-05-08 11:07:05 -0300

    adds and Missions component and missions data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Header from './components/Header';
+import Missions from './components/Missions';
 import SolarSystem from './components/SolarSytem';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
       <Header />
       <main>
         <SolarSystem />
+        <Missions />
       </main>
     </>
   );
---
+++ src/components/Missions.js
@@ -0,0 +1,14 @@
+import { Component } from 'react';
+import Title from './Title';
+
+class Missions extends Component {
+  render() {
+    return (
+      <div className="missions">
+        <Title headline="Missions" />
+      </div>
+    );
+  }
+}
+
+export default Missions;
