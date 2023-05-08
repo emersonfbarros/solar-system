77cff5fc8e62d15182de6ee9a767f736e0446c96
emersonfbarros <emersonfb99@gmail.com>
2023-05-08 11:16:35 -0300

    feat: adds and render footer

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import Footer from './components/Footer';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSytem';
@@ -11,6 +12,7 @@ function App() {
         <SolarSystem />
         <Missions />
       </main>
+      <Footer />
     </>
   );
 }
