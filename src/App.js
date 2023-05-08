009e31ad544784b4a0996c8408ae759dc166100c
emersonfbarros <emersonfb99@gmail.com>
2023-05-08 11:22:03 -0300

    adds style for the page in App.css

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import './App.css';
 import Footer from './components/Footer';
 import Header from './components/Header';
 import Missions from './components/Missions';
 import SolarSystem from './components/SolarSytem';
+import './App.css';
 
 function App() {
   return (
